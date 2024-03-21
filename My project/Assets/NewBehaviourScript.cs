dce2e54cb44de78d4849b72d5448529bb3d3a32b
Juyeon1109 <wndus2884@naver.com>
2024-03-21 19:09:04 +0900

    신나고 재밌는 특강

---
+++ My project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("잠에 들지 않으면 우린 춤을 출거에요");
+    }
+}
